e535eff519171203738752e6631999698067c028
Sanat <mister.sherim@gmail.com>
2025-07-16 21:09:40 +0500

    First Init

---
+++ src/main/java/com/sonik/practicum/repository/UserRepo.java
@@ -0,0 +1,11 @@
+package com.sonik.practicum.repository;
+
+import com.sonik.practicum.models.User;
+
+import java.util.List;
+
+public interface UserRepo {
+    List<User> findAll();
+    void save(User user);
+    void deleteById(Long id);
+}
---
+++ src/main/java/com/sonik/practicum/service/UserService.java
@@ -0,0 +1,29 @@
+package com.sonik.practicum.service;
+
+import com.sonik.practicum.models.User;
+import com.sonik.practicum.repository.UserRepo;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserService {
+    private final UserRepo userRepo;
+
+    public UserService(UserRepo userRepo) {
+        this.userRepo = userRepo;
+    }
+
+    public List<User> findAll() {
+        return userRepo.findAll();
+    }
+
+
+    public void save(User user) {
+        userRepo.save(user);
+    }
+
+    public void deleteById(Long id) {
+        userRepo.deleteById(id);
+    }
+}
