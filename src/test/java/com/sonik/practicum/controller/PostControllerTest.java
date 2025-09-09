cd79e4c1d44f4ea16c1cc484529a7a3cd5f57742
Sanat <mister.sherim@gmail.com>
2025-09-09 20:16:28 +0500

    test: add integration tests

--- src/test/java/com/sonik/practicum/controller/PostControllerTest.java
+++ src/test/java/com/sonik/practicum/controller/PostControllerTest.java
@@ -1,4 +1,31 @@
 package com.sonik.practicum.controller;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+@ExtendWith(SpringExtension.class)
 public class PostControllerTest {
+
+    @MockitoBean
+    private PostController controller;
+
+    private MockMvc mockMvc;
+
+
+    @BeforeEach
+    void setUp() {
+        InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+        viewResolver.setPrefix("classpath:templates/");
+        viewResolver.setSuffix(".html");
+
+        mockMvc = MockMvcBuilders.standaloneSetup(controller)
+                .setViewResolvers(viewResolver)
+                .build();
+    }
+
 }
