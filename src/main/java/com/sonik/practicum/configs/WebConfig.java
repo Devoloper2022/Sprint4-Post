1d39f23b2c31cd12ea42515c8652d6648ecd7a95
Sanat <mister.sherim@gmail.com>
2025-09-09 19:57:14 +0500

    fix: Problem with configs. delete DBConfig + add application properties for init DB + change location of  webConfig

--- src/main/java/com/sonik/practicum/configs/WebConfig.java
+++ src/main/java/com/sonik/practicum/configs/WebConfig.java
@@ -1,11 +1,9 @@
-package com.sonik.practicum;
+package com.sonik.practicum.configs;
 
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
-import org.springframework.web.multipart.support.StandardServletMultipartResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -21,13 +19,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/images/**")
-//                .addResourceLocations("classpath:/static/");
-//                .addResourceLocations("/static/");
         .addResourceLocations("file:" + storePath);
     }
 
-//    @Bean
-//    public StandardServletMultipartResolver multipartResolver() {
-//        return new StandardServletMultipartResolver();
-//    }
 }
